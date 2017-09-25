3302a166018ea0a8aec7a4c275582e1fc014ec22
Phillip <phillip@Phillips-Mac.local>
2017-09-24 20:21:14 -0600

    Update to react-native 0.47.0

---
+++ index.ios.js
@@ -0,0 +1,18 @@
+//import libraries to help create a component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList'
+
+//create component
+const App = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Header headerText={'Albums'} />
+      <AlbumList />
+    </View>
+  );
+};
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
